28ad5a8557f55593cbda0ab3d2137934f6429a9a
Massimo Luraschi <massimo.lura@gmail.com>
2024-12-04 15:09:40 +0100

    Added github repo link to main page

--- src/SSVClusterBalanceFetcher.js
+++ src/SSVClusterBalanceFetcher.js
@@ -32,7 +32,7 @@ const SSVClusterBalanceFetcher = () => {
 
     let url
     let daoAddress
-    if(testnet) {
+    if (testnet) {
       url = holesky_url
       daoAddress = "0x38A4794cCEd47d3baf7370CcC43B560D3a1beEFA"
     } else {
@@ -97,11 +97,11 @@ const SSVClusterBalanceFetcher = () => {
         });
 
         const responseData = await response.json();
-        const cumulativeNetworkFee = parseInt(responseData.data.daovalues.networkFeeIndex) + (responseData.data._meta.block.number - parseInt(responseData.data.daovalues.networkFeeIndexBlockNumber)) * parseInt(responseData.data.daovalues.networkFee) - (responseData.data.cluster.networkFeeIndex*10000000);
+        const cumulativeNetworkFee = parseInt(responseData.data.daovalues.networkFeeIndex) + (responseData.data._meta.block.number - parseInt(responseData.data.daovalues.networkFeeIndexBlockNumber)) * parseInt(responseData.data.daovalues.networkFee) - (responseData.data.cluster.networkFeeIndex * 10000000);
         let cumulativeOperatorFee = -responseData.data.cluster.index * 10000000;
 
         for (let operator of responseData.data.operators) {
-            cumulativeOperatorFee += parseInt(operator.feeIndex) + (responseData.data._meta.block.number - parseInt(operator.feeIndexBlockNumber)) * parseInt(operator.fee)
+          cumulativeOperatorFee += parseInt(operator.feeIndex) + (responseData.data._meta.block.number - parseInt(operator.feeIndexBlockNumber)) * parseInt(operator.fee)
         }
 
         const opFee = responseData.data.operators.reduce(
@@ -112,7 +112,7 @@ const SSVClusterBalanceFetcher = () => {
         // Calculate cluster balance
         let currentClusterArray = clusterArray
         let calculatedClusterBalance
-        if(responseData.data.cluster.validatorCount > 0){
+        if (responseData.data.cluster.validatorCount > 0) {
           calculatedClusterBalance = responseData.data.cluster.balance - (cumulativeNetworkFee + cumulativeOperatorFee) * responseData.data.cluster.validatorCount;
         } else {
           calculatedClusterBalance = responseData.data.cluster.balance - (cumulativeNetworkFee + cumulativeOperatorFee)
@@ -125,7 +125,7 @@ const SSVClusterBalanceFetcher = () => {
         let runwaySSV = calculatedClusterBalance - LC
         const operationalRunway = runwaySSV / burnRate
 
-        const currentCluster = [operatorIds.join(","), (calculatedClusterBalance/1000000000000000000).toFixed(3), operationalRunway.toFixed(0)]
+        const currentCluster = [operatorIds.join(","), (calculatedClusterBalance / 1000000000000000000).toFixed(3), operationalRunway.toFixed(0)]
         currentClusterArray.push(currentCluster)
         setClusterArray(currentClusterArray)
       }
@@ -139,90 +139,99 @@ const SSVClusterBalanceFetcher = () => {
   };
 
   return (
-<div className="flex justify-center items-center min-h-screen bg-[#0E0E52]">
-  <div className="bg-[#0E0E52] shadow-md rounded-lg p-8 w-full max-w-md">
-    <div className="flex justify-center mb-6">
-      <img src="https://ssv.network/wp-content/uploads/2024/09/Symbol.png" alt="SSV Network Logo" className="h-21 w-16" />
-    </div>
+    <div>
+      <div className="fixed top-3 right-5">
+        <a href="https://github.com/taylorferran/cluster-balance-tool" target="_blank">
+          <img
+            src="github-mark-white.svg"
+            alt="GitHub Logo"
+            className="h-8"
+          />
+        </a>
+      </div>
+      <div className="flex justify-center items-center min-h-screen bg-[#0E0E52]">
+        <div className="bg-[#0E0E52] shadow-md rounded-lg p-8 w-full max-w-md">
+          <div className="flex justify-center mb-6">
+            <img src="https://ssv.network/wp-content/uploads/2024/09/Symbol.png" alt="SSV Network Logo" className="h-21 w-16" />
+          </div>
 
-    <h2 className="text-3xl font-bold text-center mb-6 text-white">Cluster Balance Check</h2>
+          <h2 className="text-3xl font-bold text-center mb-6 text-white">Cluster Balance Check</h2>
 
-    <div className="flex justify-center items-center my-6 mx-5">
-      <span className="text-white font-medium mr-4">Mainnet</span>
-      <Switch
-        checked={testnet}
-        onChange={setTestnet}
-        className={`${testnet ? 'bg-blue-600' : 'bg-blue-600'}
+          <div className="flex justify-center items-center my-6 mx-5">
+            <span className="text-white font-medium mr-4">Mainnet</span>
+            <Switch
+              checked={testnet}
+              onChange={setTestnet}
+              className={`${testnet ? 'bg-blue-600' : 'bg-blue-600'}
           relative inline-flex h-[21px] w-[45px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75`}
-      >
-        <span
-          aria-hidden="true"
-          className={`${testnet ? 'translate-x-6' : 'translate-x-0'}
+            >
+              <span
+                aria-hidden="true"
+                className={`${testnet ? 'translate-x-6' : 'translate-x-0'}
             pointer-events-none inline-block h-[17px] w-[17px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
-      <span className="text-white font-medium ml-4">Holesky</span>
-    </div>
+              />
+            </Switch>
+            <span className="text-white font-medium ml-4">Holesky</span>
+          </div>
 
-    <div className="flex justify-center mb-4">
-      <input 
-        onChange={(event) => fetchData(event.target.value)}
-        placeholder="Paste Account Address..."
-        className="w-[300px] px-4 py-2 border border-gray-500 rounded-lg bg-[#0E0E52] text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
-    </div>
+          <div className="flex justify-center mb-4">
+            <input
+              onChange={(event) => fetchData(event.target.value)}
+              placeholder="Paste Account Address..."
+              className="w-[300px] px-4 py-2 border border-gray-500 rounded-lg bg-[#0E0E52] text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
 
-    <div className="flex justify-center items-center">
-      {loading && (
-        <div className="flex justify-center items-center space-x-2">
-          <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
-          <span className="text-blue-400 font-medium">Fetching balances...</span>
+          <div className="flex justify-center items-center">
+            {loading && (
+              <div className="flex justify-center items-center space-x-2">
+                <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
+                <span className="text-blue-400 font-medium">Fetching balances...</span>
+              </div>
+            )}
+          </div>
+
+          {tableFull !== false && (
+            <div className="w-full overflow-visible">
+              <div className="mx-auto max-w-full">
+                <table className="w-full bg-[#0E0E52] table-auto text-center">
+                  <thead>
+                    <tr className="bg-[#0E0E52] text-white">
+                      <th className="px-4 py-2 whitespace-nowrap">Operator IDs</th>
+                      <th className="px-4 py-2 whitespace-nowrap">Cluster Balance</th>
+                      <th className="px-4 py-2 whitespace-nowrap">Runway</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {clusterArray.map((cluster, index) => (
+                      <tr key={index} className="text-white">
+                        <td
+                          className="px-4 py-2"
+                          style={{
+                            wordBreak: 'break-word',
+                            maxWidth: '200px',
+                            textAlign: 'center',
+                          }}
+                        >
+                          {cluster[0]}
+                        </td>
+                        <td className="px-4 py-2 break-words">{cluster[1]} SSV</td>
+                        <td className="px-4 py-2">
+                          {(cluster[2] / blocksPerDay).toFixed(0)} days ({cluster[2]} blocks)
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+
+          {error && <p className="text-red-500 text-center mb-2">{error}</p>}
+
         </div>
-      )}
+      </div>
     </div>
-
-    {tableFull !== false && (
-  <div className="w-full overflow-visible">
-    <div className="mx-auto max-w-full">
-      <table className="w-full bg-[#0E0E52] table-auto text-center">
-        <thead>
-          <tr className="bg-[#0E0E52] text-white">
-            <th className="px-4 py-2 whitespace-nowrap">Operator IDs</th>
-            <th className="px-4 py-2 whitespace-nowrap">Cluster Balance</th>
-            <th className="px-4 py-2 whitespace-nowrap">Runway</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clusterArray.map((cluster, index) => (
-            <tr key={index} className="text-white">
-              <td
-                className="px-4 py-2"
-                style={{
-                  wordBreak: 'break-word',
-                  maxWidth: '200px',
-                  textAlign: 'center',
-                }}
-              >
-                {cluster[0]}
-              </td>
-              <td className="px-4 py-2 break-words">{cluster[1]} SSV</td>
-              <td className="px-4 py-2">
-                {(cluster[2] / blocksPerDay).toFixed(0)} days ({cluster[2]} blocks)
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-)}
-
-
-    {error && <p className="text-red-500 text-center mb-2">{error}</p>}
-    
-  </div>
-</div>
-
 
   );
 };
